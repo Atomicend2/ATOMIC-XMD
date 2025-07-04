813015707b6d7f7ba5217fd01c4ec1350e84fec2
Atomicend2 <mrmaize2@gmail.com>
2025-07-04 21:26:08 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# ATOMIC-XMD Bot
+Custom WhatsApp MD bot with games, anime-themed UI, and auto features.
---
+++ settings.js
@@ -0,0 +1,6 @@
+// Anime theme settings
+module.exports = {
+  botName: 'ATOMIC-XMD',
+  animeTheme: true,
+  welcomeTemplate: '👋 Welcome to the group, @user!',
+};
